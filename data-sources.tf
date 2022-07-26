4cdadc81a79919a441d0276ddc67bb780db3c169
Khadeer007 <110025279+Khadeer007@users.noreply.github.com>
2022-07-26 15:30:30 +0530

    Updated data-sources.tf

--- data-sources.tf
+++ data-sources.tf
@@ -1,12 +1,3 @@
 data "aws_availability_zones" "available" {
   state = "available"
 }
-
-data "terraform_remote_state" "foo" {
-  backend = "local"
-
-  config = {
-    path = "${path.module}/../../terraform.tfstate"
-  }
-}
-
